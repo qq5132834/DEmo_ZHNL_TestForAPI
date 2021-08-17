df6ac68075a0f7641f34b7a75ee7ae4f91f970c8
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2021-08-17 15:42:55 +0800

    个人简易开户

--- src/main/java/com/lon/lin/test/SxBankPersonOpenAccountTest.java
+++ src/main/java/com/lon/lin/test/SxBankPersonOpenAccountTest.java
@@ -29,32 +29,44 @@ public class SxBankPersonOpenAccountTest {
         requestData.put("secondChannelCode","");//二级平台编码
         requestData.put("tranSeqNum", UUID.randomUUID().toString().replace("-",""));//平台测测流水号
         requestData.put("certTypeCode","0");//证件类型 0：身份证；
-        requestData.put("linkedAggrType","01");//鉴权类型  01:认证绑定 02:代扣协议 (如无特殊要求，请传01)
-
         requestData.put("certNo","33010019900530156X");//证件号 一期传身份证件号码
-        requestData.put("customerName","伊丽莎白");//账号名称 传开户本人身份证上的姓名
-        requestData.put("linkedBankFlag","02");//绑定卡本他行标志 01：本行卡 02：他行卡 （湖南三湘银行卡请传01，其余传02）
-        requestData.put("linkedAcctBankCode","102100099996");//绑定账户开户行行号 102100099996-323551000015（每个银行唯一对应的银行行号，通过3.4.2行名行号接口可以查询）
-        requestData.put("linkedAcctBankName","中国工商银行");//绑定账户开户行行名 当linkedBankFlag为02则必输（每个银行都有自己的银行名称，例如中国工商银行）
-        requestData.put("linkedBaseAcctNo","6222357005000008");//绑定账户账号 绑定的银行卡号，例如：6212261904001484271
-        requestData.put("linkedAcctName","伊丽莎白");//绑定账户名称
+        requestData.put("customerName","伊丽莎白");//账号名称 传开户本人身份证上的姓名、
+
         requestData.put("teleNo","18100000004");//绑定手机号
         requestData.put("smsCode","111101");//验证码  每次开户提交前需要先通过3.4.1获取短信验证码接口获取验证码然后填入此字段，例如：235464
 
-        JSONObject imageList = new JSONObject();//审核图像数组  开户提交前需要调用3.4.3图片上传接口把身份证件上传，然后银行会返回一个图片地址，此数组用来放身份证件照片类型和地址数组
-        imageList.put("imageType","01");// 图片类型 01：身份证人像面 02：身份证国徽面 03：证件影像件（非身份证时必输） 04：纸质材料（非身份证时必输） 05：法人身份证人像面  06：法人身份证国徽面 07：代办人身份证人像面 08：代办人身份证国徽面  09：营业执照影像
-        imageList.put("imageUrl","http://10.7.1.88:8080/image/20210316/0040002-OPENAPI001-20210316112943-035460/image/伊丽莎白身份证正面.jpg");//图片地址
-        JSONObject imageList2 = new JSONObject();//审核图像数组  开户提交前需要调用3.4.3图片上传接口把身份证件上传，然后银行会返回一个图片地址，此数组用来放身份证件照片类型和地址数组
-        imageList2.put("imageType","02");// 图片类型 01：身份证人像面 02：身份证国徽面 03：证件影像件（非身份证时必输） 04：纸质材料（非身份证时必输） 05：法人身份证人像面  06：法人身份证国徽面 07：代办人身份证人像面 08：代办人身份证国徽面  09：营业执照影像
-        imageList2.put("imageUrl","http://10.7.1.88:8080/image/20210316/0040002-OPENAPI001-20210316113135-035504/image/伊丽莎白身份证反面.jpg");//图片地址
+        /**
+         * 0235-个人普通账户, linkedAggrType 设置为01认证绑定
+         * 0244-个人简易账户，linkedAggrType 设置为00不绑卡
+         */
+        requestData.put("acctType", "0244"); //0235-个人普通账户, 0244-个人简易账户;（不传则默认0235，需上送影像图片）
 
-        ArrayList list =new ArrayList<JSONObject>();
-        list.add(imageList);
-        list.add(imageList2);
+        /**
+         * 银行卡信息
+         */
+        requestData.put("linkedAggrType","00");//鉴权类型 00-首次不绑卡  01:认证绑定 02:代扣协议 (如无特殊要求，请传01)
+//        requestData.put("linkedBankFlag","02");//绑定卡本他行标志 01：本行卡 02：他行卡 （湖南三湘银行卡请传01，其余传02）
+//        requestData.put("linkedAcctBankCode","102100099996");//绑定账户开户行行号 102100099996-323551000015（每个银行唯一对应的银行行号，通过3.4.2行名行号接口可以查询）
+//        requestData.put("linkedAcctBankName","中国工商银行");//绑定账户开户行行名 当linkedBankFlag为02则必输（每个银行都有自己的银行名称，例如中国工商银行）
+//        requestData.put("linkedBaseAcctNo","6222357005000008");//绑定账户账号 绑定的银行卡号，例如：6212261904001484271
+//        requestData.put("linkedAcctName","伊丽莎白");//绑定账户名称
 
-        requestData.put("imageList",list);
 
-        requestData.put("acctType", "0235"); //0235-个人普通账户, 0244-个人简易账户;（不传则默认0235，需上送影像图片）
+        /***
+         * 身份证影像信息
+         */
+//        JSONObject imageList = new JSONObject();//审核图像数组  开户提交前需要调用3.4.3图片上传接口把身份证件上传，然后银行会返回一个图片地址，此数组用来放身份证件照片类型和地址数组
+//        imageList.put("imageType","01");// 图片类型 01：身份证人像面 02：身份证国徽面 03：证件影像件（非身份证时必输） 04：纸质材料（非身份证时必输） 05：法人身份证人像面  06：法人身份证国徽面 07：代办人身份证人像面 08：代办人身份证国徽面  09：营业执照影像
+//        imageList.put("imageUrl","http://10.7.1.88:8080/image/20210316/0040002-OPENAPI001-20210316112943-035460/image/伊丽莎白身份证正面.jpg");//图片地址
+//        JSONObject imageList2 = new JSONObject();//审核图像数组  开户提交前需要调用3.4.3图片上传接口把身份证件上传，然后银行会返回一个图片地址，此数组用来放身份证件照片类型和地址数组
+//        imageList2.put("imageType","02");// 图片类型 01：身份证人像面 02：身份证国徽面 03：证件影像件（非身份证时必输） 04：纸质材料（非身份证时必输） 05：法人身份证人像面  06：法人身份证国徽面 07：代办人身份证人像面 08：代办人身份证国徽面  09：营业执照影像
+//        imageList2.put("imageUrl","http://10.7.1.88:8080/image/20210316/0040002-OPENAPI001-20210316113135-035504/image/伊丽莎白身份证反面.jpg");//图片地址
+//
+//        ArrayList list =new ArrayList<JSONObject>();
+//        list.add(imageList);
+//        list.add(imageList2);
+//
+//        requestData.put("imageList",list);
 
         ExecuteRequest.executeRequest(requestData.toJSONString(), path,new JDEncryptAndDecrypt());
     }
